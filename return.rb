a3f49c0a8f6e44a93b8977b533a66d8e1bcaa222
Douglas Grund <douglasgrund@yahoo.com.br>
2024-02-28 15:11:47 -0300

    return

--- return.rb
+++ return.rb
@@ -1,10 +1,17 @@
-def compare (a, b)
-  a > b
+#def compare (a, b)
+#  a > b
+#end
+
+#a = 1
+#b = 2
+
+#result = compare
+
+#puts "O resultado da comparação é '#{result}' "
+
+def retorno
+  return 12
+  puts "Hello"
 end
 
-a = 1
-b = 2
-
-result = compare(a, b)
-
-puts "O resultado da comparação é '#{result}' "
+puts retorno
