0f4d556ff33e0a929760360818373a994edb9ac5
Douglas Grund <douglasgrund@yahoo.com.br>
2024-03-04 16:34:51 -0300

    Enunciado da missao

---
+++ POO/Aula6_missao1.rb
@@ -0,0 +1,22 @@
+=begin
+CRIE UMA PROGRAMA SEGUINDO O SEGUINTE PARADIGMA:after =
+classes
+
+Esportista
+  Métodos
+  competir -> Imprime a mensagem: Participando de uma competição
+
+JogadorDeFutebol
+  Métodos
+  correr -> Imprime a mensagem: "Correndo atrás da bola"
+
+Maratonista
+  Métodos
+  correr -> Imprime a mensagem: "Percorrendo o circuito"
+
+
+ESSAS CLASSES JogadorDeFutebol E Maratonista DEVEM HERDAR OS
+COMPORTAMENTOS DA CLASSE Espostita.
+NO FINAL DO PROGRAMA EXECUTE OS MÉTODOS competir E correr EM
+OBJETOS DO TIPO JogadorDeFutebol E Maratonista.
+=end
