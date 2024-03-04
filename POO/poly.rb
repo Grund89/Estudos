a7a6ca5f00074c32e9c712060023b048ee54742a
Douglas Grund <douglasgrund@yahoo.com.br>
2024-03-04 15:47:47 -0300

    Criado 3 clases

---
+++ POO/poly.rb
@@ -0,0 +1,19 @@
+class Instrumento
+  def escrever
+    puts 'Escrevendo'
+  end
+end
+
+class Teclado < Instrumento
+end
+
+class Lapis < Instrumento
+  def escrever
+    puts 'Escrevendo à Lápis'
+  end
+end
+
+class Caneta < Instrumento
+  def escrever
+    puts 'Escrevendo à Caneta'
+  end
