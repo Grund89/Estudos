362b762f4f514008f15df0d496198f7a311ae4a8
Douglas Grund <douglasgrund@yahoo.com.br>
2024-03-04 15:36:23 -0300

    2 classes

---
+++ POO/animal.rb
@@ -0,0 +1,20 @@
+class Animal
+  def pular
+    puts 'Toing! Toim! Boim! Poim!'
+  end
+
+  def dormir
+    puts 'ZzZzzz!'
+  end
+end
+
+
+class Cachorro < Animal
+  def latir
+    puts 'Au au'
+  end
+end
+
+cachorro = Cachorro.new
+cachorro.pular
+cachorro.dormir
