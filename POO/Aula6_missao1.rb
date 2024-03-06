068dc34202d6528a53dccd44d3f1bef19895688d
Douglas Grund <douglasgrund@yahoo.com.br>
2024-03-06 15:55:33 -0300

    Missao realizada

--- POO/Aula6_missao1.rb
+++ POO/Aula6_missao1.rb
@@ -20,3 +20,31 @@ COMPORTAMENTOS DA CLASSE Espostita.
 NO FINAL DO PROGRAMA EXECUTE OS MÉTODOS competir E correr EM
 OBJETOS DO TIPO JogadorDeFutebol E Maratonista.
 =end
+
+class Esportista
+  def competir
+    puts "Participando de uma competição"
+  end
+end
+
+class JogadorDeFutebol < Esportista
+  def correr
+    puts "Correndo atrás da bola"
+  end
+end
+
+class Maratonista < Esportista
+  def correr
+    puts "Percorrendo o circuito"
+  end
+end
+
+# Criando objetos e chamando os métodos
+jogador = JogadorDeFutebol.new
+maratonista = Maratonista.new
+
+jogador.competir
+jogador.correr
+
+maratonista.competir
+maratonista.correr
