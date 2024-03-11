5f12c95a894578d34197fd74d835eea8ee61038c
Douglas Grund <douglasgrund@yahoo.com.br>
2024-03-11 15:59:29 -0300

    ajustado frase dentro do print

--- variaveis/local.rb
+++ variaveis/local.rb
@@ -1,6 +1,6 @@
 def foo
   #Pode ser definida como local ou _local
-  local = 'Local é acessada apenas desntro deste metodo'
+  local = 'Local é acessada apenas desse deste metodo'
   print local
 end
 
