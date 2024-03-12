8d3f447e2b092a80e73583a1a3a87f4e44f2998d
Douglas Grund <douglasgrund@yahoo.com.br>
2024-03-12 11:22:40 -0300

    new program

--- variaveis/atributos.rb
+++ variaveis/atributos.rb
@@ -13,3 +13,14 @@ dog = Dog.new
 dog.name = 'Marlon'
 puts dog.name
 =end
+
+class Dog
+  attr_accessor :name, :age
+end
+
+dog = Dog.new
+dog.name = 'Marlon'
+puts dog.name
+
+dog.age = '1 ano'
+puts dog.age
