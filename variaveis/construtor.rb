367f46b4000a2cc82e1f6f9a30be3cf043708988
Douglas Grund <douglasgrund@yahoo.com.br>
2024-03-12 11:50:17 -0300

    finished program

--- variaveis/construtor.rb
+++ variaveis/construtor.rb
@@ -3,3 +3,13 @@ class Person
     @name = name
     @age = age
   end
+
+  def check
+    puts "Instância da classe iniciada com os valores:"
+    puts "Name = #{@name}"
+    puts "Idade = #{@age}"
+  end
+end
+
+person = Person.new('João', 12)
+peron.check
